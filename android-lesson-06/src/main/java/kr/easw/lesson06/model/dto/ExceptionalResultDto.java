c32933280538f7e512114252e39457b0e1a10668
CHOHYUNJUN (조현준) <124676369+HyunJJJUN@users.noreply.github.com>
2023-12-03 17:24:14 +0900

    Add files via upload

---
+++ android-lesson-06/src/main/java/kr/easw/lesson06/model/dto/ExceptionalResultDto.java
@@ -0,0 +1,3 @@
+package kr.easw.lesson06.model.dto;
+
+public record ExceptionalResultDto(String message) { }
---
+++ android-lesson-06/src/main/java/kr/easw/lesson06/model/dto/RemoveUserDto.java
@@ -0,0 +1,12 @@
+package kr.easw.lesson06.model.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class RemoveUserDto {
+    private String userId;
+
+}
